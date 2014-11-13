36e6d8a2c72c1b4156eb4dfeccc4519283e946e7
mcbrumagin <mcbrumagin@gmail.com>
2014-11-13 01:16:24 -0500

    Html for testing
    An html/php partial file to include in a layout for testing.

---
+++ jaskell-html-test.php
@@ -0,0 +1,12 @@
+<div id="test-pane" class="test-pane">
+	<a href="http://www.sitepoint.com/jquery-vs-raw-javascript-1-dom-forms/">Native JS alternatives to jQuery</a>
+	<div class="test0 test">Test div 0</div>
+	<div class="thing test1">Test div 1</div>
+	<div class="est test2 neat">Test div 2</div>
+	<div class="est   test3  neat ">Test div 3</div>
+	<div class="test4 est est estest neat">Test div 4</div>
+	<div class="   test5 neat est">Test div 5</div>
+	<div class="test6 neat  est ">Test div 6</div>
+	<button id="btn">Test</button>
+	<span>Create event listener/trigger framework</span>
+</div>
