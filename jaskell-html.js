5cc31106f54cc7a615eceea89b247d7e72bb3b25
mcbrumagin <mcbrumagin@gmail.com>
2014-11-14 00:51:56 -0500

    Event create/trigger
    Event creation, triggering, and an example.
    Also reformatted code.

--- jaskell-html.js
+++ jaskell-html.js
@@ -1,4 +1,4 @@
-var len = function(obj) {
+var len = function (obj) {
     return obj ? obj.length : -1
 }
 
@@ -6,22 +6,22 @@ function isFunction(f) {
     return {}.toString.call(f) === '[object Function]'
 }
 
-var toArray = function(args) {
+var toArray = function (args) {
     return Array.prototype.slice.call(args)
 }
 
-var sequence = function() {
+var sequence = function () {
     var fns = arguments,
         l = len(arguments)
     console.log(l)
     console.log(fns)
-    var seq = function() {
+    var seq = function () {
         var i = -1
         var args = toArray(arguments)
         while (i++ < l - 1) {
             if (!isFunction(fns[i])) {
                 var val = fns[i]
-                fns[i] = function() {
+                fns[i] = function () {
                     return val
                 }
             }
@@ -34,7 +34,7 @@ var sequence = function() {
 }
 
 // Util
-var each = function(objs, fn) {
+var each = function (objs, fn) {
     console.log('For each: ')
     console.log(objs)
     if (objs.length) {
@@ -57,74 +57,100 @@ var html = {
         tag: document.getElementsByTagName.bind(document),
         name: document.getElementsByName.bind(document)
     },
-    append: function(val, elem) {
-        var append = function(o) {
+    append: function (val, elem) {
+        var append = function (o) {
             o.innerHTML += val;
             return o
         }
-        if (!elem) return function(elem) {
+        if (elem) return each(elem, append)
+        else return function (elem) {
             return each(elem, append)
-        } else return each(elem, append)
+        }
     },
     class: {
-        add: function(val, elem) {
-                var add = function(o) {
-                    var ind = o.className.indexOf(val)
-                    if (ind > -1) return o
-                    else {
-                        var classes = o.className.split(' ').concat(val.split(' '))
-                        var resultClasses = []
-                        for (var i = 0; i < len(classes); i++) {
-                            if (classes[i] != '') resultClasses.push(classes[i])
-                        }
-                        o.className = resultClasses.join(' ')
-                    }
-                    return o
-                }
-                if (!elem) return function(elem) {
-                    return each(elem, add)
-                } else return each(elem, add)
-            },
-            remove: function(val, elem) {
-                var rem = function(o) {
-                    var classes = o.className.split(' ')
+        add: function (val, elem) {
+            var add = function (o) {
+                var ind = o.className.indexOf(val)
+                if (ind > -1) return o
+                else {
+                    var classes = o.className.split(' ').concat(val.split(' '))
                     var resultClasses = []
-                    for (var i = 0; i < classes.length; i++) {
-                        if (classes[i] != val && classes[i] != '') resultClasses.push(classes[i])
+                    for (var i = 0; i < len(classes); i++) {
+                        if (classes[i] != '') resultClasses.push(classes[i])
                     }
                     o.className = resultClasses.join(' ')
-                    return o
                 }
-                if (!elem) return function(elem) {
-                    return each(elem, rem)
-                } else return each(elem, rem)
+                return o
             }
+            if (elem) return each(elem, add)
+			else return function (elem) {
+				return each(elem, add)
+			}
+        },
+        remove: function (val, elem) {
+            var rem = function (o) {
+                var classes = o.className.split(' ')
+                var resultClasses = []
+                for (var i = 0; i < classes.length; i++) {
+                    if (classes[i] != val && classes[i] != '') resultClasses.push(classes[i])
+                }
+                o.className = resultClasses.join(' ')
+                return o
+            }
+            if (elem) return each(elem, rem)
+            else return function (elem) {
+                return each(elem, rem)
+            }
+        }
     },
     event: {
-        listen: function(type, handler, elem) {
-            var bind = function(elem) {
+        listen: function (type, handler, elem) {
+            var bind = function (elem) {
                 elem.addEventListener(type, handler, false)
                 return elem
             }
             if (elem) return each(elem, bind)
-            else return function(elem) {
+            else return function (elem) {
                 return each(elem, bind)
             }
         },
-        capture: function(type, handler, elem) {
-            var bind = function(elem) {
+        capture: function (type, handler, elem) {
+            var bind = function (elem) {
                 elem.addEventListener(type, handler, true)
                 return elem
             }
             if (elem) return each(elem, bind)
-            else return function(elem) {
+            else return function (elem) {
                 return each(elem, bind)
             }
         },
-        watch: function(type, handler, target) {}
+        watch: function (type, handler, target) {
+        },
+        list: {},
+        create: function (name, bubbles, cancelable, detail) {
+            var options = {}
+            options.bubbles = bubbles === undefined ? true : bubbles
+            options.cancelable = cancelable === undefined ? true : cancelable
+            html.event.list[name] = function () {
+                if (detail) options.detail = detail()
+                return new CustomEvent(name, options)
+            }
+        },
+        trigger: function (event, elem) {
+            var trigger = function (elem) {
+                var evt = html.event.list[event]
+                if (evt) elem.dispatchEvent(evt())
+                else elem.dispatchEvent(html.event.create(event)())
+                return elem
+            }
+            if (elem) return each(elem, trigger)
+            else return function (elem) {
+                return each(elem, trigger)
+            }
+        }
     },
-    log: function(message, elem) {
-        var log = function(elem) {
+    log: function (message, elem) {
+        var log = function (elem) {
             console.log(message, elem)
             return elem
         }
@@ -133,13 +159,16 @@ var html = {
     }
 }
 
-var using = function(nameSpace, operations) {
+var using = function (nameSpace, operations) {
     return operations(nameSpace)
 }
 
 
 using(html, function operations(_) {
-
+    var iterator = 0;
+    _.event.create('test', true, true, function () {
+        return {date: new Date(), iteration: iterator++}
+    })
     _.append('!', _.select.id('test-pane'))
 
     var append123 = sequence(
@@ -151,13 +180,13 @@ using(html, function operations(_) {
         _.class.remove('test0'))
 
     _.class.remove('thing', _.select.class('test1'))
-        //_.class.remove('test2', _.select.class('test2'))
+    //_.class.remove('test2', _.select.class('test2'))
 
     sequence(
         _.select.class('neat'),
         _.class.remove('est'),
         _.class.add('  woah     dude  '),
-        _.event.capture('click', function(event) {
+        _.event.capture('click', function (event) {
             event.preventDefault()
             using(event.currentTarget,
                 sequence(
@@ -168,12 +197,19 @@ using(html, function operations(_) {
         }),
         _.append('...'),
         _.select.class('test5'),
-        _.append('???'))
+        _.append('???'),
+        _.event.capture('click', function () {
+            _.event.trigger('test', document.body)
+        }))
+
+    _.event.capture('test', function (event) {
+        console.log(event.detail)
+    }, document.body)
 
     sequence(
         _.select.tag('button'),
         _.append(' Button'),
-        _.event.capture('click', function(event) {
+        _.event.capture('click', function (event) {
             event.preventDefault()
             alert('neat')
             append123('test0')
@@ -182,42 +218,42 @@ using(html, function operations(_) {
 });
 
 /*
-//jQuery euivalent
-$(document).ready(function() {
+ //jQuery euivalent
+ $(document).ready(function() {
 
-    $('#test-pane').append('!')
+ $('#test-pane').append('!')
 
-    var append123 = function(sel) {
-        $(sel)
-            .append('1')
-            .append('2')
-            .append('3')
-            .addClass('neat')
-            .removeClass('test0')
-    }
+ var append123 = function(sel) {
+ $(sel)
+ .append('1')
+ .append('2')
+ .append('3')
+ .addClass('neat')
+ .removeClass('test0')
+ }
 
-    $('.test1').removeClass('thing')
+ $('.test1').removeClass('thing')
 
-    $('.neat')
-        .removeClass('est')
-        .addClass('   woah    dude  ')
-        .on('click', function(event) {
-            event.preventDefault()
-            $(event.currentTarget)
-                .append('..')
-                .append('?')
-            console.log('appending', event.currentTarget)
-        })
-        .append('...')
+ $('.neat')
+ .removeClass('est')
+ .addClass('   woah    dude  ')
+ .on('click', function(event) {
+ event.preventDefault()
+ $(event.currentTarget)
+ .append('..')
+ .append('?')
+ console.log('appending', event.currentTarget)
+ })
+ .append('...')
 
-    $('.test5').append('???')
+ $('.test5').append('???')
 
-    $('button')
-        .append('Button')
-        .on('click', function(event) {
-            event.preventDefault()
-            alert('neat')
-            append123('.test0')
-        })
-})
-*/
+ $('button')
+ .append('Button')
+ .on('click', function(event) {
+ event.preventDefault()
+ alert('neat')
+ append123('.test0')
+ })
+ })
+ */
